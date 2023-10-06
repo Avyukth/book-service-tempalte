0ba7433c85783b41f08f4c17cdaee6142364c1e1
Avyukth <makur.subhrajit@gmail.com>
2023-10-06 23:01:51 +0530

    adding graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod rest;
 use crate::db::init_db;
 use std::env;
 use std::net::{IpAddr, SocketAddr};
+use tokio::signal;
 
 use anyhow::{Ok, Result};
 use axum::{Extension, Router};
@@ -28,8 +29,44 @@ async fn main() -> Result<()> {
     let app = router(connection_pool);
 
     let addr = SocketAddr::from((host.parse::<IpAddr>()?, port));
+
+    // Create a future that listens for shutdown signal
+    let shutdown_signal = shutdown_signal();
+
+    println!("Server running on http://{}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal)
         .await?;
+
+    println!("Server shutting down");
+
     Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
